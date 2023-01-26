9fc900038ea689fdea26ffc226a354530096d542
Oleksandr Zozulia <freemanlex@gmail.com>
2023-01-26 10:03:15 +0000

    upd

--- verification/tests.py
+++ verification/tests.py
@@ -11,41 +11,41 @@ TESTS = {
     "Basics": [
         {
             "input": [["1.cad", "1.bat", "1.aa"]],
-            "answer": ["1.aa", "1.bat", "1.cad"]
+            "answer": ["1.aa", "1.bat", "1.cad"],
         },
         {
             "input": [["1.cad", "1.bat", "1.aa", "2.bat"]],
-            "answer": ["1.aa", "1.bat", "2.bat", "1.cad"]
+            "answer": ["1.aa", "1.bat", "2.bat", "1.cad"],
         },
         {
             "input": [["1.cad", "1.bat", "1.aa", ".bat"]],
-            "answer": [".bat", "1.aa", "1.bat", "1.cad"]
+            "answer": [".bat", "1.aa", "1.bat", "1.cad"],
         },
         {
             "input": [[ "1.cad", "1.bat", ".aa", ".bat"]],
-            "answer": [".aa", ".bat", "1.bat", "1.cad"]
+            "answer": [".aa", ".bat", "1.bat", "1.cad"],
         },
         {
             "input": [[ "1.cad", "1.", "1.aa"]],
-            "answer": ["1.", "1.aa", "1.cad"]
+            "answer": ["1.", "1.aa", "1.cad"],
         },
         {
             "input": [["1.cad", "1.bat", "1.aa", "1.aa.doc"]],
-            "answer": ["1.aa", "1.bat", "1.cad", "1.aa.doc"]
+            "answer": ["1.aa", "1.bat", "1.cad", "1.aa.doc"],
         },
         {
             "input": [["1.cad", "1.bat", "1.aa", ".aa.doc"]],
-            "answer": ["1.aa", "1.bat", "1.cad", ".aa.doc"]
-        }
+            "answer": ["1.aa", "1.bat", "1.cad", ".aa.doc"],
+        },
     ],
     "Extra": [
         {
             "input": [[".config", "my.doc", "1.exe", "345.bin", "green.bat", "format.c", "no.name.", "best.test.exe"]],
-            "answer": [".config", "no.name.", "green.bat", "345.bin", "format.c", "my.doc", "1.exe", "best.test.exe"]
+            "answer": [".config", "no.name.", "green.bat", "345.bin", "format.c", "my.doc", "1.exe", "best.test.exe"],
         },
         {
             "input": [["1.cad", "2.bat", "1.aa", "1.bat"]],
-            "answer": ["1.aa", "1.bat", "2.bat", "1.cad"]
-        }
+            "answer": ["1.aa", "1.bat", "2.bat", "1.cad"],
+        },
     ]
 }
